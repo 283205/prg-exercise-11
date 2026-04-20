02e558c2f3411844ef6ea2da9aaa52cb4fd073bd
Anna Zahradnickova <283205@vutbr.cz>
2026-04-20 13:23:24 +0200

    2 a 3 cast

--- sorting.py
+++ sorting.py
@@ -3,7 +3,35 @@ import random
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
-values = random_numbers(10)  # 10 čísel v rozsahu 0–100
-print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
+def selection_sort(values):
+
+    for min_index in range(len(values)):
+        print(values)
+        min_value = values[min_index]
+        min_ind = min_index
+        for i in range(min_index, len(values)):
+            if values[i] < min_value:
+                min_ind = i
+                min_value = values[i]
+        values[min_ind], values[min_index] = values[min_index], values[min_ind]
+    print(values)
+
+def bubble_sort(values):
+    print(values)
+    for i in range(len(values)): #říká nám jak dlouho budeme dělat
+        for j in range(0, len(values) - 1): #porovnáva dva prvky vedle sebe
+
+            if values[j] > values[j + 1]:
+                values[j], values[j + 1] = values[j + 1], values[j]
+
+    print(values)
+
+
+
+if __name__ == "__main__":
+    values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    print(bubble_sort(values))  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
 
 small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
+
+
