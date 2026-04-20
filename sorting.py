78a1b67c588f2cc6807f081e9f1728d55505ac22
Anna Zahradnickova <283205@vutbr.cz>
2026-04-20 13:30:41 +0200

    vykleslení bubble sort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -18,12 +19,24 @@ def selection_sort(values):
 
 def bubble_sort(values):
     print(values)
+    plt.ion()
+    plt.show()
     for i in range(len(values)): #říká nám jak dlouho budeme dělat
         for j in range(0, len(values) - 1): #porovnáva dva prvky vedle sebe
 
             if values[j] > values[j + 1]:
                 values[j], values[j + 1] = values[j + 1], values[j]
-
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+    plt.ioff()
+    plt.show()
     print(values)
 
 
